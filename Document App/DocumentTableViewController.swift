53ea36a970ee63836625eebe6d7e7d6102a70b68
Ahmed-Omer <Ahmed.Omerovic@etu.univ-savoie.fr>
2024-01-17 12:04:00 +0100

    images s'affichent

--- Document App/DocumentTableViewController.swift
+++ Document App/DocumentTableViewController.swift
@@ -67,19 +67,12 @@ func listFileInBundle() -> [DocumentFile] {
 }
 
 
-class DocumentTableViewController: UITableViewController, QLPreviewControllerDataSource {
-    
+class DocumentTableViewController: UITableViewController {
     var selectedDocumentURL: URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
+
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DocumentCell")
     }
     
@@ -96,97 +89,38 @@ class DocumentTableViewController: UITableViewController, QLPreviewControllerDat
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "DocumentCell"
-
-        // Use dequeueReusableCell(withIdentifier:for:) to ensure a valid cell is returned
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-
-        let document = DocumentFile.documents[indexPath.row]
-
-        // Set the cell style to .subtitle
-        cell.textLabel?.text = document.title
-        cell.detailTextLabel?.text = "Size: \(document.size.formatedSize())"
-        cell.detailTextLabel?.numberOfLines = 0  // Use this line if you want multiline detail text
-
-        return cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
+            
+            let document = DocumentFile.documents[indexPath.row]
+            
+            cell.textLabel?.text = document.title
+            cell.detailTextLabel?.text = "Size: \(document.size.formatedSize())"
+            
+            return cell
+        }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let file = DocumentFile.documents[indexPath.row]
+        selectedDocumentURL = file.url
+        showQLPreviewController()
     }
 
-    
-    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Vérifie si sender est UITableViewCell et si l'index path peut être récupéré
-     if let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell) {
-     
-     // Vérifie si destination du segue est instance de DocumentViewController
-     if let documentViewController = segue.destination as? DocumentViewController {
-     
-     // Récupérer document correspondant à index
-     let selectedDocument = DocumentFile.documents[indexPath.row]
-     
-     // Assigner nom au DocumentViewController
-     documentViewController.imageName = selectedDocument.imageName
-     }
-     }
-     }*/
-    
-    
+    func showQLPreviewController() {
+        guard let documentURL = selectedDocumentURL else {
+            return
+        }
+
+        let previewController = QLPreviewController()
+        previewController.dataSource = self
+        navigationController?.pushViewController(previewController, animated: true)
+    }
+}
+extension DocumentTableViewController: QLPreviewControllerDataSource {
+
     func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
-        return selectedDocumentURL != nil ? 1 : 0
+        return 1
     }
-    
+
     func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
         return selectedDocumentURL! as QLPreviewItem
     }
-    
-    // Ajouter cette méthode pour résoudre le problème
-    func previewController(_ controller: QLPreviewController, titleForPreviewItemAt index: Int) -> String? {
-        return selectedDocumentURL?.lastPathComponent
-    }
-    
-    
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
